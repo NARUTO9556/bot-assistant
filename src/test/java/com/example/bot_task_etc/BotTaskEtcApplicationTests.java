4fea1a7a79e0deaac34f43baa097a135fc881332
NARUTO9556 <gruver5263@gmail.com>
2025-05-26 01:09:43 +0800

    initial commit

---
+++ src/test/java/com/example/bot_task_etc/BotTaskEtcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bot_task_etc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotTaskEtcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
